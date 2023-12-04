4bd2a5d19e247262b650e4f2b2c1105589460775
Andrew Leonard <ajyey@andrewleonard.dev>
2023-12-04 00:50:58 -0500

    install python via pyenv and eza setup in server setup script

--- setup-server.sh
+++ setup-server.sh
@@ -16,48 +16,7 @@ sudo apt update && sudo apt upgrade -y
 
 # Install common packages
 echo "Installing common packages..."
-sudo apt install -y vim git curl wget ufw fail2ban htop zsh
-
-# Install Oh My Zsh
-echo "Installing Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo "Setting zsh as the default shell..."
-chsh -s $(which zsh)
-
-echo "Installing plugins..."
-# Install zsh-autosuggestions
-echo "Installing zsh-autosuggestions..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-# Install zsh-syntax-highlighting
-echo "Installing zsh-syntax-highlighting..."
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# Add plugins to .zshrc
-echo "Adding zsh-autosuggestions and zsh-syntax-highlighting to .zshrc..."
-sed -i '/^plugins=(git)/ a plugins+=(zsh-autosuggestions zsh-syntax-highlighting)' ~/.zshrc
-
-# Set Git username and email using the provided environment variables
-echo "Setting Git username and email..."
-git config --global user.name "$GIT_USER"
-git config --global user.email "$GIT_EMAIL"
-# Set Vim as the default Git editor
-git config --global core.editor "vim"
-
-# Install pyenv
-echo "Installing pyenv..."
-curl https://pyenv.run | bash
-
-# The following lines are added to .zshrc instead of .bashrc for Zsh configuration
-echo "Setting up pyenv..."
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(pyenv init --path)"' >> ~/.zshrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
-# Apply the changes to the current session
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv virtualenv-init -)"
+sudo apt install -y vim git curl wget ufw fail2ban htop zsh gpg
 
 # Set up a firewall with UFW
 echo "Setting up the firewall..."
@@ -70,6 +29,62 @@ echo "Installing and configuring SSH..."
 sudo apt install -y openssh-server
 sudo systemctl enable ssh
 sudo systemctl start ssh
+
+# Install Oh My Zsh
+echo "Installing Oh My Zsh..."
+# Set CHSH to no to prevent the script from prompt me to change the default shell
+CHSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Setting zsh as the default shell..."
+sudo chsh -s $(which zsh) $USER
+
+echo "Installing plugins..."
+# Install zsh-autosuggestions
+echo "Installing zsh-autosuggestions..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Install zsh-syntax-highlighting
+echo "Installing zsh-syntax-highlighting..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# Add plugins to .zshrc
+echo "Adding zsh-autosuggestions and zsh-syntax-highlighting to .zshrc..."
+sed -i '/^plugins=(git)/ a plugins+=(zsh-autosuggestions zsh-syntax-highlighting)' ~/.zshrc
+
+# https://eza.rocks/
+echo "Installing and configuring Eza..."
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt update
+sudo apt install -y eza
+# Add alias to .zshrc for ls command so that it uses eza
+echo 'alias ls="eza --header --long"' >> ~/.zshrc
+
+
+# Set Git username and email using the provided environment variables
+echo "Setting Git username and email..."
+git config --global user.name "$GIT_USER"
+git config --global user.email "$GIT_EMAIL"
+# Set Vim as the default Git editor
+git config --global core.editor "vim"
+
+# Install pyenv
+echo "Installing pyenv..."
+curl https://pyenv.run | bash
+
+
+echo "Setting up pyenv..."
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+# Reload .zshrc
+source ~/.zshrc
+# Install python
+pyenv install 3.12.0 
+pyenv global 3.12.0   # Set the installed version as the global default
+# Upgrade pip to its latest version
+pip install --upgrade pip
+
 
 # Install Docker
 echo "Installing Docker and Docker Compose..."
@@ -92,11 +107,13 @@ sudo usermod -aG docker $USER
 
 # Output Docker and Docker Compose versions
 echo "Docker and Docker Compose installed:"
-docker --version
-docker-compose --version
+docker version
+docker compose version
+
+# Reload .zshrc
+source ~/.zshrc
 
 # Additional setup can go here...
 
 echo "Home server setup script has completed."
 echo "You will need to log out and back in for the usermod and default shell changes to take effect."
-echo "Please restart your shell or run 'source ~/.zshrc' to apply pyenv and Oh My Zsh settings."
