dd958b5281a390763c3030ac8dbe3ac77e473ce4
Andrew Leonard <ajyey@andrewleonard.dev>
2023-12-04 23:12:14 -0500

    fix: type on server setup script

--- setup-server.sh
+++ setup-server.sh
@@ -16,7 +16,7 @@ sudo apt update && sudo apt upgrade -y
 
 # Install common packages
 echo "Installing common packages..."
-sudo apt-get install -y make vim git tree curl wgethtop ufw zsh gpg build-essential libssl-dev zlib1g-dev \
+sudo apt-get install -y make vim git tree curl wget htop ufw zsh gpg build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncurses-dev xz-utils tk-dev libffi-dev liblzma-dev
 
